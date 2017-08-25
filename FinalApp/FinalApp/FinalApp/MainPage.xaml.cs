9852bc5a85b28da7a37552e0e28e4da1a8378b97
Matthew-Harding <matthew.sourcecode@gmail.com>
2017-08-25 13:33:54 +0100

    initial commit

---
+++ FinalApp/FinalApp/FinalApp/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FinalApp
+{
+    public partial class MainPage : ContentPage
+    {
+        App _appClass;
+
+        string correctUserName = "s";
+        string correctPassword = "s";
+
+        public MainPage(App appClass)
+        {
+            _appClass = appClass;
+            InitializeComponent();
+           
+
+        }
+
+        private void Login()
+        {
+            _appClass.MainPage = new NavigationPage(new Page02(_appClass));
+        }
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            bool userNameFilled = false;
+            bool passwordFilled = false;
+
+            //Checking which textboxes are empty
+            if (UsernameInput.Text != null && UsernameInput.Text != string.Empty)
+                userNameFilled = true;
+            if (PasswordInput.Text != null && PasswordInput.Text != string.Empty)
+                passwordFilled = true;
+
+            //If the username and password textbox contains text
+            if(userNameFilled && passwordFilled)
+            {
+                if(UsernameInput.Text == correctUserName && PasswordInput.Text == correctPassword)
+                {
+                    //Login
+                    Login();
+                } else
+                    DisplayAlert("Warning", "Username/Password Incorrect", "OK");
+            }
+            else if (!userNameFilled && !passwordFilled)
+                DisplayAlert("Warning", "Please Specify a Username and Password", "OK");
+
+            else if(!userNameFilled)
+                DisplayAlert("Warning", "Please Specify a Username", "OK");
+
+            else
+                DisplayAlert("Warning", "Please Specify a Password", "OK");
+        }
+    }
+}
---
+++ FinalApp/FinalApp/FinalApp/Page02.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FinalApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page02 : ContentPage
+	{
+        App _appClass;
+
+		public Page02 (App appClass)
+		{
+            _appClass = appClass;
+			InitializeComponent ();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private void LogoutButton_Clicked(object sender, EventArgs e)
+        {
+            _appClass.MainPage = new MainPage(_appClass);
+        }
+    }
+}
